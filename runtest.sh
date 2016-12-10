42c84ddf4f1208d45fb8418d6204c6708a179578
DanoBuck <DaniielBuckleyTY3@gmail.com>
2016-12-10 15:17:18 +0000

    Work done on runtest.sh so far. Not working with the loadtest

--- runtest.sh
+++ runtest.sh
@@ -2,9 +2,15 @@
 
 echo "Starting to gather CPU data"
 
+echo "CO N idle" > results.dat
 for i in {1..50}
 do
-	mpstat | grep "idle" >> out.txt
-	#awk '{print $0}'
+	# Run the loadtest each time and increase the number of users
+	./loadtest $i  &
+	idle=$(mpstat | awk 'END{print $NF}')
+	C0=$(cat synthetic.dat | wc -l)
+	mpstat >> mpstat.txt
+	echo $C0 $i $idle
+	echo "$C0   $i $idle" >> results.dat
 	
 done
